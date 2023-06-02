4a3e48776092618f732185033ce4e668be030a65
sagar Salunke <sagarsalunke08@gmail.com>
2023-06-02 14:23:50 +0000

    Add my new java File

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
-public Static void main(String[] args){
-System.out.println ("Hello,word!");
-}
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello, world!");
+   }
 }
